1ea410a4be68fe88d4cc35779e3f50d22f853fea
alesi <apinto@ing.ucsc.cl>
2022-10-21 12:17:16 +0000

    visita inicial

--- main.py
+++ main.py
@@ -3,6 +3,27 @@ import numpy as np
 import sys
 import math
 
+def RegistroCiudad(hormiga,ciudad,ciudadesVisitadas):
+   ciudadesVisitadas[hormiga][ciudad] = 1
+   return ciudadesVisitadas
+
+
+
+def visitaInicial(hormigas_size,numVariables):
+   trayectoriahormiga = np.full((hormigas_size,numVariables),1)
+   ciudadesVisitadas = np.full((hormigas_size,numVariables),0)
+   for i in range(0,hormigas_size):
+      ciudad = np.random.randint(numVariables,dtype=int)
+      trayectoriahormiga[i][0] = ciudad
+      ciudadesVisitadas = RegistroCiudad(i,ciudad,ciudadesVisitadas)
+      
+   print(trayectoriahormiga,ciudadesVisitadas)
+
+   return trayectoriahormiga,ciudadesVisitadas
+
+
+
+
 # py .\main.py berlin52.txt 10 10 100 0.1 2.5 0.9
 
 if len(sys.argv) == 8 :
@@ -29,8 +50,13 @@ np.random.seed(seed=seed)
 coordenadas = np.genfromtxt(archivo_entrada,delimiter=" ",skip_footer=1,skip_header=6,dtype=int)
 numVariables = coordenadas.shape[0]
 print(coordenadas)
-Distancia = np.full((coordenadas.shape[0],coordenadas.shape[0]),-1,dtype=float)
 
+trayectoriahormiga,ciudadesVisitadas = visitaInicial(hormigas_size,numVariables)
+
+""" print(ciudadvisitada)
+
+Distancia = np.full((coordenadas.shape[0],coordenadas.shape[0]),-1,dtype=float)
+#print(Distancia)
 for i in range(0,len(Distancia)):
    for j in range(i+1,len(Distancia)):
       distancia = math.sqrt(math.pow(coordenadas[i][1]-coordenadas[j][1],2)+math.pow(coordenadas[i][2]-coordenadas[j][2],2))
@@ -40,8 +66,13 @@ for i in range(0,len(Distancia)):
 Heuristica = np.full_like(Distancia,fill_value=1/Distancia,dtype=float)
 solucionMejor = np.arange(0,coordenadas.shape[0])
 np.random.shuffle(solucionMejor)
-solucionMejorCosto = solucionCalculaCosto(numVariables,solucionMejor,Distancia)
-Tij0 = 1/(numVariables*solucionMejorCosto)
-matrizFeromona = np.full_like(Distancia,fill_value=Tij0,dtype=float)
-print(matrizFeromona)
-print(solucionMejorCosto)
+solucionMejorCosto = solucionCalculaCosto(numVariables,solucionMejor,Distancia) """
+#Tij0 = 1/(numVariables*solucionMejorCosto)
+#matrizFeromona = np.full_like(Distancia,fill_value=Tij0,dtype=float)
+#print(matrizFeromona)
+#print(solucionMejorCosto)
+
+
+
+
+
